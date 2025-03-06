ced3273067d4748ed763b7a26360580568d26bdb
DAVIDMWELU <davidmwelu@gmail.com>
2025-03-06 13:10:49 +0300

    component mounting lab

--- src/__tests__/Timer.test.js
+++ src/__tests__/Timer.test.js
@@ -3,9 +3,11 @@ import { configure, shallow } from 'enzyme';
 import { spy, stub, useFakeTimers } from 'sinon'
 import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter() });
+
 
 import Timer from '../Timer';
+
+configure({ adapter: new Adapter() });
 
 test('it calls componentDidMount', () => {
   spy(Timer.prototype, 'componentDidMount');
